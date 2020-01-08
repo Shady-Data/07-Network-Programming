145e6ecddc8116eb4d083dc2cb0037c6189c913f
root <root@localhost.localdomain>
2020-01-08 12:04:55 -0500

    Replaced old code in studentcode/challengelabs with the working code

--- studentcode/challenge_labs/full-duplex_client.py
+++ studentcode/challenge_labs/full-duplex_client.py
@@ -1,39 +1,40 @@
 #!/usr/bin/env python3
 
 import socket
-from random import randint
-import threading
-from time import sleep
+import concurrent.futures
 
 HOST = 'localhost'
 PORT = 13373
 ADDR = (HOST, PORT)
 BUFF = 1024
 
+def client_send(conn):
+    while True:
+        msg = input(' > ')
+        if not msg:
+            msg = 'shutdown'
+            conn.send(msg.encode())
+            break
+        conn.send(msg.encode())
+    # print('>>>Send loop broken<<<')
+
+def client_receive(conn):
+    while True:
+        data = conn.recv(BUFF)
+        if data.decode('utf-8') == 'shutdown':
+            print('shutdown received')
+            break
+        print(' <', data.decode('utf-8'))
+
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect(ADDR)
 
-def client_send():
-    try:
-        while True:
-            msg = input(' > ')
-            if not msg:
-                break
-            client.send(msg.encode())
-    except KeyboardInterrupt:
-        return
+with concurrent.futures.ThreadPoolExecutor() as executor: 
+    t1 = executor.submit(client_send, client.dup())
+    t2 = executor.submit(client_receive, client.dup())
 
-def client_receive():
-    try:
-        while True:
-            data = client.recv(BUFF)
-            print(data.decode('utf-8'))
-    except KeyboardInterrupt:
-        return
-
-t1 = threading.Thread(target=client_send, name=3)
-t2 = threading.Thread(target=client_receive, name=4)
-
-t1.start()
-t2.start()
-
+    while True:
+        if t1.done() and t2.done():
+            # print('t1 and t2 complete')
+            client.close()
+            break
--- studentcode/challenge_labs/full-duplex_server.py
+++ studentcode/challenge_labs/full-duplex_server.py
@@ -2,8 +2,31 @@
 
 import socket
 import datetime
-import threading
+import concurrent.futures
 from time import sleep
+
+def server_send(conn):
+    while True:
+        msg = input('send >')
+        if not msg or msg == 'shutdown':
+            conn.send(msg.encode())
+            break
+        elif msg == '!time':
+            msg = f'The current time is {datetime.datetime.now().strftime("%A, %B %d, %Y %X")}'
+        conn.send(msg.encode())
+    print('send loop broken')
+    conn.close()
+
+def server_receive(conn):
+    while True:
+        data = conn.recv(BUFF)
+        if data.decode('utf-8') == 'shutdown' or not data:
+            msg = 'shutdown'
+            conn.send(msg.encode())
+            break
+        print(' <', data.decode('utf-8'))
+    print('receive loop broken')
+    conn.close()
 
 HOST = 'localhost'
 PORT = 13373
@@ -18,27 +41,14 @@ print('Server Running')
 try:
     while True:
         conn, addr = srvr.accept()
-
-        def server_send():
+        with concurrent.futures.ThreadPoolExecutor() as executor: 
+            t1 = executor.submit(server_send, conn.dup())
+            t2 = executor.submit(server_receive, conn.dup())
             while True:
-                # msg = input('send >')
-                msg = f'The current time is {datetime.datetime.now().strftime("%A, %B %d, %Y %X")}'
-                conn.send(msg.encode())
-                sleep(5)
-
-        def server_receive():
-            while True:
-                data = conn.recv(BUFF)
-                if not data:
+                if t1.done() or t2.done():
+                    print('threads report done')
+                    conn.close()
                     break
-                print(data.decode('utf-8'))
-
-        t1 = threading.Thread(target=server_send, name=1)
-        t2 = threading.Thread(target=server_receive, name=2)
-
-        t1.start()
-        t2.start()
-
 
 except KeyboardInterrupt:
     print('Interrupt recieved. Shutting server down')
